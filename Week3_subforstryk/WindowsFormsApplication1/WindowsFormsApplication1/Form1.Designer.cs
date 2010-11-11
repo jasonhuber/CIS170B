644ad7a33a6643d90f9162fbaaa77bd9450b70b2
Jason Huber <jasonhuber@gmail.com>
2010-11-10 19:10:04 -0700

    Added some initial code. if and some refactoring.

---
+++ Week3_subforstryk/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(71, 6);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(71, 50);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(70, 131);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 2;
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(14, 9);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(35, 13);
+            this.lblNum1.TabIndex = 3;
+            this.lblNum1.Text = "Num1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Num2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 93);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(24, 23);
+            this.btnGo.TabIndex = 6;
+            this.btnGo.Text = "+";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(96, 93);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(24, 23);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(57, 93);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(24, 23);
+            this.btnMinus.TabIndex = 8;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(135, 93);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(24, 23);
+            this.btnMult.TabIndex = 9;
+            this.btnMult.Text = "*";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 162);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMult;
+    }
+}
+
---
+++ Week3_subforstryk/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            double number1, number2;
+
+            if (CheckInputs(txtNum1.Text, txtNum2.Text, out number1, out number2))
+            {
+                txtResult.Text = (number1 + number2).ToString();
+            }
+            else 
+            {
+                txtNum1.ForeColor = System.Drawing.Color.Red;
+                txtResult.Text = "invalid entry";
+            }
+        }
+
+        private bool CheckInputs(string inNum1, string inNum2, out double number1, out double number2)
+        {
+            number1 = 0;
+            number2 = 0;
+            if (double.TryParse(inNum1, out number1) &&
+                double.TryParse(inNum2, out number2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            double number1, number2;
+
+            if (CheckInputs(txtNum1.Text, txtNum2.Text, out number1, out number2))
+            {
+                txtResult.Text = (number1 - number2).ToString();
+            }
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            double number1, number2;
+
+            if (CheckInputs(txtNum1.Text, txtNum2.Text, out number1, out number2))
+            {
+                if (number2 != 0)
+                {
+                    txtResult.Text = (number1 / number2).ToString();
+                    
+                }
+            }
+
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            double number1, number2;
+
+            if (CheckInputs(txtNum1.Text, txtNum2.Text, out number1, out number2))
+            {
+                txtResult.Text = (number1 * number2).ToString();
+            }
+        }
+
+    }
+}
